9f94a9070fe5d6c05f03be7619c24a7743ee0d57
DevExpressExampleBot <fakeEmail>
2020-01-20 19:44:10 +0300

    Source auto update [skip ci]

--- VB/Filter_Master_Detail_Example/Filter_Master_Detail_Example/Form1.vb
+++ VB/Filter_Master_Detail_Example/Filter_Master_Detail_Example/Form1.vb
@@ -13,22 +13,22 @@ Imports System.Threading.Tasks
 Imports System.Windows.Forms
 
 Namespace Filter_Master_Detail_Example
-    Partial Public Class Form1
-        Inherits XtraForm
+	Partial Public Class Form1
+		Inherits XtraForm
 
-        Public Sub New()
-            InitializeComponent()
-            AddHandler dashboardDesigner1.ConfigureDataConnection, AddressOf DashboardDesigner1_ConfigureDataConnection
-            dashboardDesigner1.UseNeutralFilterMode = True
-            dashboardDesigner1.CreateRibbon()
-            dashboardDesigner1.LoadDashboard("CascadeFiltersDashboard.xml")
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+			AddHandler dashboardDesigner1.ConfigureDataConnection, AddressOf DashboardDesigner1_ConfigureDataConnection
+			dashboardDesigner1.UseNeutralFilterMode = True
+			dashboardDesigner1.CreateRibbon()
+			dashboardDesigner1.LoadDashboard("CascadeFiltersDashboard.xml")
+		End Sub
 
-        Private Sub DashboardDesigner1_ConfigureDataConnection(ByVal sender As Object, ByVal e As DevExpress.DashboardCommon.DashboardConfigureDataConnectionEventArgs)
-            Dim parameters As ExtractDataSourceConnectionParameters = TryCast(e.ConnectionParameters, ExtractDataSourceConnectionParameters)
-            If parameters IsNot Nothing Then
-                parameters.FileName = Path.GetFileName(parameters.FileName)
-            End If
-        End Sub
-    End Class
+		Private Sub DashboardDesigner1_ConfigureDataConnection(ByVal sender As Object, ByVal e As DevExpress.DashboardCommon.DashboardConfigureDataConnectionEventArgs)
+			Dim parameters As ExtractDataSourceConnectionParameters = TryCast(e.ConnectionParameters, ExtractDataSourceConnectionParameters)
+			If parameters IsNot Nothing Then
+				parameters.FileName = Path.GetFileName(parameters.FileName)
+			End If
+		End Sub
+	End Class
 End Namespace
